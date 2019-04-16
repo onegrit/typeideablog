ffb0536aae5297a668c3b73a6caf4c63b079b4f5
OneGrit <2012joe@gmail.com>
2019-04-16 10:49:23 +0800

    add blogApp and commentApp

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogApp.apps.BlogappConfig',
+    'commentApp.apps.CommentappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
